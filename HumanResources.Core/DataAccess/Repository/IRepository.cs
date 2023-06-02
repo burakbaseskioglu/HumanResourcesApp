30acf986fc4d0c8a862f3850acea469864b48f8e
BurakBaseskioglu <burakbaseskioglu@gmail.com>
2023-06-03 01:16:35 +0300

    initial create

---
+++ HumanResources.Core/DataAccess/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using HumanResources.Entity.Abstract;
+using System.Linq.Expressions;
+
+namespace HumanResources.Core.DataAccess.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T Get(Expression<Func<T, bool>> expression);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> expression = null);
+    }
+}
---
+++ HumanResources.Entity/Abstract/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources.Entity.Abstract
+{
+    public abstract class BaseEntity
+    {
+
+    }
+}
